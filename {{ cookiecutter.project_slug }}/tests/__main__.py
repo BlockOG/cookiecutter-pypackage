abe32b7f8e9ceb5aa8ad2c01b65ebf5c54815d11
BlockOfGaming <alesiobaca@gmail.com>
2022-06-13 19:25:30 +0300

    Holy that was bad

--- {{ cookiecutter.project_slug }}/tests/__main__.py
+++ {{ cookiecutter.project_slug }}/tests/__main__.py
@@ -1,8 +1,10 @@
 import src.{{ cookiecutter.project_slug }} as {{ cookiecutter.project_slug }}, unittest
 
+
 class TestEverything(unittest.TestCase):
     def test_something(self):
         self.assertTrue(True)
+
 
 if __name__ == "__main__":
     unittest.main()
